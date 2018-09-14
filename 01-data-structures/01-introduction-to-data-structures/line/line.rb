a950a0de8ab332e7f3a4a3cfce049d4053e2b88e
Andrew Delapaz <ardelapaz@yahoo.com>
2018-09-13 20:25:48 -0400

    Finished line.rb

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -5,30 +5,53 @@ class Line
   attr_accessor :members
 
   def initialize
-    self.members = []
+    return self.members = []
   end
 
   def join(person)
+    return self.members.push(person)
   end
 
   def leave(person)
+    self.members.each_with_index do |member, index|
+      if (member == person) 
+        self.members.delete_at(index)
+      end
+    end
   end
 
   def front
+    return self.members[0]
   end
 
   def middle
+    middle = (self.members.length / 2)
+    return self.members[middle]
   end
 
   def back
+    last = (self.members.length - 1)
+    return self.members[last]
   end
 
   def search(person)
+    self.members.each do |member|
+      if (member == person) 
+        return member 
+      end
+    end
+    return nil
   end
 
   private
 
   def index(person)
+    self.members.each do |member|
+      if (member == person) 
+        return member 
+      end
+    end
+    return nil
   end
 
 end
