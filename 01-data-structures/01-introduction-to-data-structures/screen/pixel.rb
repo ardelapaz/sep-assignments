fc9f14991ef6c32200633af5ed34533435489890
Andrew Delapaz <ardelapaz@yahoo.com>
2018-09-19 18:27:40 -0400

    Finished part 1

--- 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/pixel.rb
@@ -12,11 +12,22 @@ class Pixel
 
 
   def initialize(red, green, blue, x, y)
+    @red = validate_color(red)
+    @green = validate_color(green)
+    @blue = validate_color(blue)
+    @x = validate_color(x)
+    @y = validate_color(y)
   end
 
   private
 
   def validate_color(color)
+    if (color > 255)
+      return 255
+    elsif (color < 0)
+      return 0
+    else return color
+    end
   end
 
 end
